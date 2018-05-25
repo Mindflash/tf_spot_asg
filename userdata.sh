a2e390dec3fc62878964028668b81ccf4a051005
charles-d-burton <charles.burton@mindflash.com>
2018-05-25 15:36:38 -0600

    fix: get key.pem file

--- userdata.sh
+++ userdata.sh
@@ -2,3 +2,4 @@
 echo ${env} > /etc/mf-env
 mkdir /etc/mf-secrets
 aws s3 cp s3://mf-pa-secrets/${env}/secrets.js /etc/mf-secrets/secrets.js
+aws s3 cp s3://mf-pa-secrets/${env}/key.pem /etc/mf-secrets/key.pem
