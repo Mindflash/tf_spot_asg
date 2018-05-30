57785694a738223d40d3e47f48d8f328a57c9199
charles-d-burton <charles.burton@mindflash.com>
2018-05-30 12:04:31 -0600

    fix: userdata updated for key.pem

--- userdata.sh
+++ userdata.sh
@@ -3,3 +3,5 @@ echo ${env} > /etc/mf-env
 mkdir /etc/mf-secrets
 aws s3 cp s3://mf-pa-secrets/${env}/secrets.js /etc/mf-secrets/secrets.js
 aws s3 cp s3://mf-pa-secrets/${env}/key.pem /etc/mf-secrets/key.pem
+cp /etc/mf-secrets/key.pem /home/ubuntu/node/key.pem
+chown ubuntu:ubuntu /home/ubuntu/node/key.pem
