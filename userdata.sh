ea38c6ac59a6034423055b863500928757029e94
charles-d-burton <charles.burton@mindflash.com>
2018-05-31 17:46:39 -0600

    fixes

--- userdata.sh
+++ userdata.sh
@@ -3,5 +3,7 @@ echo ${env} > /etc/mf-env
 mkdir /etc/mf-secrets
 aws s3 cp s3://mf-pa-secrets/${env}/secrets.js /etc/mf-secrets/secrets.js
 aws s3 cp s3://mf-pa-secrets/${env}/key.pem /etc/mf-secrets/key.pem
-cp /etc/mf-secrets/key.pem /home/ubuntu/node/key.pem
-chown ubuntu:ubuntu /home/ubuntu/node/key.pem
+cp /etc/mf-secrets/key.pem /home/app/node/key.pem
+chown app:app /home/app/node/key.pem
+chmod 644 /etc/mf-secrets/*
+chmod 644 /etc/mf-env
