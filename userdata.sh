9966f416ab47199a377d52d674113b044eb8eb44
charles-d-burton <charles.burton@mindflash.com>
2018-06-01 17:55:33 -0600

    fixes

--- userdata.sh
+++ userdata.sh
@@ -5,5 +5,9 @@ aws s3 cp s3://mf-pa-secrets/${env}/secrets.js /etc/mf-secrets/secrets.js
 aws s3 cp s3://mf-pa-secrets/${env}/key.pem /etc/mf-secrets/key.pem
 cp /etc/mf-secrets/key.pem /home/app/node/key.pem
 chown app:app /home/app/node/key.pem
+chmod 755 /etc/mf-secrets
 chmod 644 /etc/mf-secrets/*
 chmod 644 /etc/mf-env
+
+#Give the FS time to sync before the apps start
+sleep 2
