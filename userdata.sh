3317220b7a4d20ed16113c99103b6acd5328971f
charles-d-burton <charles.burton@mindflash.com>
2018-06-06 16:48:32 -0600

    fix: added snap path to bin

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo ${env} > /etc/mf-env
+export PATH=$PATH:/snap/bin
 mkdir /etc/mf-secrets
 aws s3 cp s3://mf-pa-secrets/${env}/secrets.js /etc/mf-secrets/secrets.js
 aws s3 cp s3://mf-pa-secrets/${env}/key.pem /etc/mf-secrets/key.pem
