280d03414aa869447c77f32f62bed49485973dfa
charles-d-burton <charles.burton@mindflash.com>
2018-06-08 10:12:31 -0600

    fix: updated userdata

--- userdata.sh
+++ userdata.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-while [ ! -f /snap/bin/aws ]
-do
- echo "Waiting for aws cli"
- sleep 1; 
-done
 
 echo ${env} > /etc/mf-env
-export PATH=$PATH:/snap/bin
 mkdir /etc/mf-secrets
-aws s3 cp s3://mf-pa-secrets/${env}/secrets.js /etc/mf-secrets/secrets.js
-aws s3 cp s3://mf-pa-secrets/${env}/key.pem /etc/mf-secrets/key.pem
+/usr/local/bin/aws s3 cp s3://mf-pa-secrets/${env}/secrets.js /etc/mf-secrets/secrets.js
+/usr/local/bin/aws s3 cp s3://mf-pa-secrets/${env}/key.pem /etc/mf-secrets/key.pem
 cp /etc/mf-secrets/key.pem /home/app/node/key.pem
 chown app:app /home/app/node/key.pem
 chmod 755 /etc/mf-secrets
